03c7db58ea466944242ec60fc38ae64ed63ec16e
Masud Rana <masud15-924@diu.edu.bd>
2022-12-07 00:07:32 +0600

    input section design

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <p>hi</p>
+        <div className="desktop-1">
+            <div className="title-section">
+                <h2>Enter your Title</h2>
+                <input type="text" />
+            </div>
+            <div className="sub-title-section">
+                <h2>Enter your SubTitle</h2>
+                <input type="text" placeholder='' />
+            </div>
+            <div className="loact-action-section">
+            <h2>Enter your SubTitle</h2>
+                <input type="text" placeholder='' />
+            </div>
+           
         </div>
     );
 };
